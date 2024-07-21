461d40fc10955e9ce361acf897ae2d1718b9bc5b
Guilherme Almeida <guidevtec@gmail.com>
2024-07-21 08:42:54 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using contatos.Models;
+
+namespace contatos.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        HomeModel home = new HomeModel();
+        home.Email = "guiadventista@gmail";
+        home.Nome = "Guilherme";
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
